239a30c48b41241d2c1175edfc3498be95d9266e
Aleksey Verin <verevaa@yandex.ru>
2023-04-13 21:55:28 +0300

    refactor: deploy problem

--- src/pages/PageCalls.tsx
+++ src/pages/PageCalls.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Info from '../components/PageCalls/Info';
-import Filtering from '../components/PageCalls/Filtering';
-import SpreadSheet from '../components/PageCalls/SpreadSheet';
+import Info from '../components/pageCalls/Info';
+import Filtering from '../components/pageCalls/Filtering';
+import SpreadSheet from '../components/pageCalls/SpreadSheet';
 
 const PageCalls = () => {
   return (
