84714ef7c46544faa1ade2d42da8a07b2b8d99c6
Aleksey Verin <verevaa@yandex.ru>
2023-05-06 23:05:49 +0300

    refactor: cleaned some code

--- src/components/header/Company.tsx
+++ src/components/header/Company.tsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import ImgArrow from '../images/ImgArrow';
-import { useSelector } from 'react-redux';
-import { selectorCallsDataSlice } from '../../store/reducers/callsDataSlice';
-import Loader from '../commonUI/Loader';
 
 const Company = () => {
-  const { data } = useSelector(selectorCallsDataSlice);
-
-  const partnerName = data ? data[0]?.partner_data.name : null;
+  // const { data } = useSelector(selectorCallsDataSlice);
+  // const partnerName = data ? data[0]?.partner_data.name : null;
 
   return (
     <div className="company">
-      <div className="company-name">{partnerName}</div>
+      <div className="company-name">ООО "ГРУЗЧИКОВ-СЕРВИС СПБ"</div>
       <ImgArrow direction="btn-down" />
     </div>
   );
